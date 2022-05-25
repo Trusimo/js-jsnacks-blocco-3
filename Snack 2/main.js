5884dc21b03712388f121d892d684b16a60eee58
Trusimo <simonetrunfio96@gmail.com>
2022-05-25 18:11:53 +0200

    human code

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -1,3 +1,12 @@
 // Fai inserire un numero, che chiameremo N, all’utente.
 // Genera N array, ognuno formato da 10 numeri casuali da 1 a 100.
 // Ogni volta che ne crei uno, stampalo.
+
+// inseriamo un numero
+var numero = prompt("Inserisci un numero");
+
+// dobbiamo generare tanti array quanto il valore della var numero (come???)
+
+// ogni array avrà Math.floor(Math.random() * 101) per 10
+
+//console.log(array)
