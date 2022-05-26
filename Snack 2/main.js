b38e5a5f3e22374a2ba03e834eaba9377fa71fb7
Trusimo <simonetrunfio96@gmail.com>
2022-05-26 10:04:14 +0200

    correzione esercizio

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -3,10 +3,23 @@
 // Ogni volta che ne crei uno, stampalo.
 
 // inseriamo un numero
-var numero = prompt("Inserisci un numero");
+var numero = parseInt(prompt("Inserisci un numero"));
+
+const arraygenerati = [];
 
 // dobbiamo generare tanti array quanto il valore della var numero (come???)
+while (arraygenerati.length < numero) {
+    const nuovoarray = [];
 
 // ogni array avrà Math.floor(Math.random() * 101) per 10
+    while(nuovoarray.length < 10) {
+        const numerocasuale = Math.floor(Math.random() * 100) + 1;
 
-//console.log(array)
+        nuovoarray.push(numerocasuale);
+    }
+
+
+    arraygenerati.push(nuovoarray);
+}
+
+console.log(arraygenerati);
